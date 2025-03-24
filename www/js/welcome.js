bd8360f4cafcebad8383ca9669132b6268000736
angela-vargas <u01a7094@lacaixa.es>
2025-03-24 16:00:25 +0100

    Adding scroll

--- www/js/welcome.js
+++ www/js/welcome.js
@@ -58,6 +58,30 @@ function initMap() {
 }
 
 
+function animateOnScroll(className) {
+  window.addEventListener('scroll', function () {
+      const elements = document.querySelectorAll(`.${className}`);
+      const windowHeight = window.innerHeight;
+      const triggerPoint = windowHeight * 0.85;
+
+      elements.forEach(element => {
+          const elementTop = element.getBoundingClientRect().top;
+          if (elementTop < triggerPoint) {
+              element.classList.add('show');
+          } else {
+              element.classList.remove('show');
+          }
+      });
+  });
+}
+
+// Llamar a la función para distintos elementos
+document.addEventListener("DOMContentLoaded", function () {
+  animateOnScroll('event');      // Para los eventos del itinerario
+  animateOnScroll('scroll-reveal'); // Para los elementos que se revelan al hacer scroll
+});
+
+
 // // Itinerario
 // window.addEventListener('scroll', function () {
 //   const events = document.querySelectorAll('.event');
