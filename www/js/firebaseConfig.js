11793c3a11e2dcd75ad25fdf88226d4077faea88
angela-vargas <u01a7094@lacaixa.es>
2025-03-31 12:24:58 +0200

    dding configbase

--- www/js/firebaseConfig.js
+++ www/js/firebaseConfig.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvzoGMS3fTCJwGP7nM_ov9gAP57hP1m00",
-  authDomain: "sarayjordiwedding.firebaseapp.com",
-  projectId: "sarayjordiwedding",
-  storageBucket: "sarayjordiwedding.firebasestorage.app",
-  messagingSenderId: "829826807273",
-  appId: "1:829826807273:web:f3f5d75f8593b31d680778",
-  measurementId: "G-VCNDK2XG49"
+  apiKey: "AIzaSyDis_-Swsd9sVlWHfKWDOqq4dCPzCole6Y",
+  authDomain: "martayalvarowedding.firebaseapp.com",
+  projectId: "martayalvarowedding",
+  storageBucket: "martayalvarowedding.firebasestorage.app",
+  messagingSenderId: "510769567699",
+  appId: "1:510769567699:web:561a0fb620505893738126"
 };
+
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
